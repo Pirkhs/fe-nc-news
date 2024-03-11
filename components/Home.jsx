6c65882ddcf011ecb275407ba52015f70d877194
Pirkhs <nathan.briers32@gmail.com>
2024-03-11 12:06:45 +0000

    added home page, header, and nav

---
+++ components/Home.jsx
@@ -0,0 +1,9 @@
+import '../src/Home.css'
+
+export default function Home () {
+    return (
+        <div className="home">
+            <h3> Welcome to NC News 📰 </h3>
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Header from '../components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Nav from '../components/Nav'
+import Home from '../components/Home'
 
 function App() {
 
 
   return (
     <>
-
+      <Header/>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
