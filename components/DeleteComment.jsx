fe346d29486504fc460568ffb3b7c30507ec97aa
Pirkhs <nathan.briers32@gmail.com>
2024-03-13 11:02:50 +0000

    added additional feedback for deleting comments

--- components/DeleteComment.jsx
+++ components/DeleteComment.jsx
@@ -1,21 +1,32 @@
 import '../styles/DeleteComment.css'
 import { deleteComment } from '../src/api'
 import Error from './Error'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Loading from './Loading'
 
-export default function DeleteComment ({comment_id, comments, handleCommentState}) {
+export default function DeleteComment ({comment_id, handleCommentState}) {
     const [error, setError] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleDeleteComment = (e) => {
+        setIsLoading(true)
+        e.target.classList.add("disabled")
         deleteComment(comment_id)
         .then(() => handleCommentState(currComments => {
             const copyCurrComments = [...currComments]
             return copyCurrComments.filter(comment => comment.comment_id !== comment_id)
         }))
-        .catch(err => setError("404: Not found"))
+        .catch(err => {
+            setError(err.message)
+            setIsLoading(false)
+        })
     }
 
-    return error? (<Error msg={error}/>) : (
+    if (isLoading) return <Loading/>
+
+    if (error) return  <Error msg={error}/>
+
+    return (
         <div className="delete-comment">
             <button onClick = {(e) => handleDeleteComment(e)}> Delete 🗑️ </button>
         </div>
